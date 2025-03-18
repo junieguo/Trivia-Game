87422240592a6a3a4b2cf0f46e0b23ba1075c4e4
junieguo <junieguo@sas.upenn.edu>
2025-03-17 21:32:25 -0400

    Update welcome page UI

--- Trivia Game/Trivia Game/Views/WelcomeView.swift
+++ Trivia Game/Trivia Game/Views/WelcomeView.swift
@@ -6,35 +6,90 @@
 import SwiftUI
 
 struct WelcomeView: View {
-    @EnvironmentObject var gameState: GameState // Access the global game state
-
+    @EnvironmentObject var gameState: GameState
+    @State private var showInstructions = false
+    
     var body: some View {
         NavigationStack {
-            VStack {
+            VStack(spacing: 20) {
+                
                 Text("World Currency Trivia")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                    .padding()
-
+                
+                Image("WorldCurrencies")
+                    .resizable()
+                    .frame(width: 400, height: 200)
+                
                 Text("Test your knowledge of currencies around the world with a series of trivia questions.")
-                    .font(.title2)
-                    .padding()
+                    .font(.title3)
+                    .padding(.horizontal)
+                
+                Spacer().frame(height: 15)
+                
+                HStack {
+                    Text("🏆 Highscore:")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                    
+                    Text("\(gameState.highscore)")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.green)
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    NavigationLink(destination: GameView(gameState: gameState)) {
+                        Text("Start Quiz")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing)
+                            )
+                            .cornerRadius(15)
+                    }
 
-                Text("Highscore: \(gameState.highscore)")
-                    .font(.headline)
-                    .padding()
-
-                NavigationLink(destination: GameView(gameState: gameState)) {
-                    Text("Start Quiz")
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                    Button(action: {
+                        showInstructions.toggle()
+                    }) {
+                        Image(systemName: "info.circle")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.leading, 5)
                 }
                 .padding()
             }
-            .navigationBarTitle("Trivia Game", displayMode: .inline)
+            .sheet(isPresented: $showInstructions) {
+                VStack {
+                    Text("Instructions")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding()
+
+                    Text("In this trivia game, you will be asked questions related to world currencies. Choose the correct answer from multiple choices before time runs out. Each correctly answered questions adds a point to the score. Try to get the highest score possible!")
+                        .font(.body)
+                        
+
+                    Button(action: {
+                        showInstructions = false
+                    }) {
+                        Text("Close")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .padding()
+                }
+                .padding()
+            }
         }
     }
 }
